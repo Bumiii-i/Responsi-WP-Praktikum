2e2f6a8b3807aa1f5a51b085c0cb90f547951958
Dihyan Gantari Bumi <dihyanxd@gmail.com>
2026-07-24 16:55:37 +0700

    First Commit
    karena pake push gak bisa, saya upload saja (terlalu malas buat trobleshoot jadi daripada lama saya upload saja)

---
+++ config.py
@@ -0,0 +1,17 @@
+class Config:
+    """
+    Konfigurasi aplikasi Flask
+    """
+
+    # Konfigurasi Database MySQL (Hosting filess.io)
+    HOST = "tt2zry.h.filess.io"
+    PORT = 3306
+    USER = "db_akademik_processadd"
+    PASSWORD = "b36bdd020b92e2550d1017020ddb342612ebbb6a"
+    DATABASE = "db_akademik_processadd"
+
+    # Secret Key Flask
+    SECRET_KEY = "praktikum-flask-2026"
+
+    # Konfigurasi Flask
+    DEBUG = True
